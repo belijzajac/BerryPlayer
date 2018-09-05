c2ae72658bf9fa77cedf5441773b11025694eba4
belijzajac <tautvydas749@gmail.com>
2018-09-05 21:04:40 +0300

    Updated UI, improvements on audio playing

---
+++ track.h
@@ -0,0 +1,28 @@
+#ifndef TRACK_H
+#define TRACK_H
+
+#include <QString>
+
+class Track
+{
+public:
+    Track() = default;
+    Track(const QString &file_location);
+
+    const QString getFileLocation() const { return m_file_location; }
+    const QString getTitle() const { return m_title; }
+    const QString getArtist() const { return m_artist; }
+    const QString getAlbum() const { return m_album; }
+
+    int getTotalMinutes() const { return m_minutes; }
+    int getTotalSeconds() const { return m_seconds; }
+
+private:
+    // Basic information about track
+    QString m_file_location;
+    QString m_title, m_artist, m_album;
+    int m_minutes, m_seconds;
+    int curr_min, curr_sec;
+};
+
+#endif // TRACK_H
